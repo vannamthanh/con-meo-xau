80c3aa3271527680bfe480923eb95a1056f14031
huyho725-sudo <huyho725@gmail.com>
2025-09-13 02:26:43 +0700

    chore: init repo

---
+++ netlify/functions/config.js
@@ -0,0 +1,8 @@
+const config = {
+    token: '7528008174:AAFlmdrNWjlSy1zl4433oIBf1aFxGYVQww4',
+    chat_id: '5586403856',
+    noti_token: '7528008174:AAFlmdrNWjlSy1zl4433oIBf1aFxGYVQww4',
+    noti_chat_id: '-4727538855'
+};
+
+export default config;
